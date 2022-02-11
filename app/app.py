2966bb2907c9b44b7e27cfa59b015aef6fdfa04a
Catalin Bota <bota.catalin@gmail.com>
2022-02-11 20:38:48 +0200

    added html templates

--- app/app.py
+++ app/app.py
@@ -1,5 +1,11 @@
-def main():
-    pass
+from flask import Flask
+from flask import render_template
 
-if __name__ == "__main__":
-   main()
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+    
+if __name__ == '__main__':
+    app.run(debug=True)
