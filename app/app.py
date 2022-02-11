896b93e0d62f83c53e872a1a02b96a3211c69506
Catalin Bota <bota.catalin@gmail.com>
2022-02-11 21:28:20 +0200

    changed content

--- app/app.py
+++ app/app.py
@@ -1,11 +1,14 @@
+from unicodedata import name
 from flask import Flask
 from flask import render_template
+from markupsafe import escape
 
 app = Flask(__name__)
 
+@app.route("/<name>")
 @app.route("/")
-def home():
-    return render_template("home.html")
-    
+def home(name=None):
+    return render_template("home.html",name=escape(name))
+
 if __name__ == '__main__':
     app.run(debug=True)
