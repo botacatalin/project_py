2f6a2fe954fac39f1dae776b1b8ae0e8b000e685
Catalin Bota <bota.catalin@gmail.com>
2022-02-12 13:10:23 +0200

    Added logging code

--- app/app.py
+++ app/app.py
@@ -2,12 +2,16 @@ from unicodedata import name
 from flask import Flask
 from flask import render_template
 from markupsafe import escape
+import logging
+from flask import request
 
 app = Flask(__name__)
+# logging.basicConfig(filename='logs/log.log', encoding='utf-8', level=logging.DEBUG)
 
 @app.route("/<name>")
 @app.route("/")
 def home(name=None):
+    # app.logger.info('app started ...')
     return render_template("home.html",name=escape(name))
 
 if __name__ == '__main__':
